dc5c5560321d69024465b65e4b85766700c192c6
Frank Lam <frank.is.missing@gmail.com>
2018-03-20 23:30:34 -0700

    Completed chapter 2 exercises

---
+++ 2-Hello-Haskell/WhereExercises.hs
@@ -0,0 +1,6 @@
+module WhereExercises where
+func1 = z / x + y
+    where x = 7
+          y = negate x
+          z = y * 10
+
---
+++ 2-Hello-Haskell/heal.hs
@@ -0,0 +1,6 @@
+module Heal where
+area x = pi * (x * x)
+double b = b * 2
+x = 7
+y = 10
+f x y = x + y
